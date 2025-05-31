e59e6695f2c307f61a703377edae432fc26101ae
NicolasAConstantin <156631305+NicolasAConstantin@users.noreply.github.com>
2025-05-30 22:30:44 -0600

    Created classes for mesurements

---
+++ OOPsReview/OOPsReview/Employment.cs
@@ -0,0 +1,173 @@
+﻿namespace OOPsReview
+{
+    public class Employment
+    {
+        //Generally private unless stated otherwise.
+        #region Attributes
+
+        private SupervisoryLevel _Level;
+        private string _Title;
+        private double _Years; //Years of Employment.
+
+        #endregion
+
+        //If we have a property that's name matches an Attribute, that is a Fully-Implemented Property.
+
+        //If we have a property that does not have a matching attribute, that is an auto-implemented property.
+
+        //Generally public unless stated otherwise.
+        #region Properties
+
+        public SupervisoryLevel Level
+        {
+            get { return _Level; }
+
+            private set
+            {
+                if(!Enum.IsDefined(typeof(SupervisoryLevel), value))
+                {
+                    throw new ArgumentException($"Supervisory Level {value} is invalid.");
+                }
+
+                _Level = value;
+            }
+        }
+
+        public DateTime StartDate { get; set; }
+
+        public string Title
+        {
+            get { return _Title; }
+            set 
+            {
+                //IsNullOrWhiteSpace checks for any purely whitespace strings.
+                //Is better than IsNullOrEmpty
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Title is Required.");
+                }
+                _Title = value;
+            }
+        }
+        public double Years
+        {
+            get { return _Years; }
+            set 
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException($"Year value {value} is invalid. Must be non-negative.");
+                }
+                _Years = value;
+            }
+        }
+
+        #endregion
+
+        //Almost always public or internal; generally match the class
+        #region Constructors
+
+        /// <summary>
+        /// Our default constructor.
+        /// </summary>
+        public Employment()
+        {
+            //Can not be null or whitespace
+            Title = "Unknown";
+
+            //Years defaults to 0, so no need to implement it here.
+            Level = SupervisoryLevel.Entry;
+
+            StartDate = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Our greedy contructor.
+        /// 
+        /// StartDate can not be in the future.
+        /// Years will calculate if not provided.
+        /// </summary>
+        /// <param name="title"> Position Title </param>
+        /// <param name="level"> SupervisoryLevel of position </param>
+        /// <param name="startDate"> Position start date </param>
+        /// <param name="years">Years of employment; default 0.0 </param>
+        /// 
+        /* years has a default value, so I can leave it out when calling the contructor if needed. This gives me 2 constructors from 1 definition.
+         * 
+         * Parameters with default values must be declared after those without defaults. I can have as many default parameters as I wish.
+         * */
+        public Employment(string title, SupervisoryLevel level, DateTime startDate, double years = 0.0)
+        {
+            Title= title;
+            Level = level;
+
+            //StartDate can not be in the future
+            //This will check if startDate is tomorrow or later.
+            if(startDate >= DateTime.Today.AddDays(1))
+            {
+                throw new ArgumentException($"The start date {startDate} is in the future.");
+            }
+            
+            //Else not necessary as the throw will exit my constructor.
+            StartDate = startDate;
+
+            if (years > 0.0)
+            {
+                Years = years;
+            }
+            else
+            {
+                TimeSpan span = DateTime.Now - startDate;
+                Years = Math.Round((span.Days / 365.25), 1); //365.25 accounts for leap years
+            }
+        }
+        #endregion
+
+        //Depends on usage, though generally public
+        #region Methods
+
+        /// <summary>
+        /// Sets this instances SupervisoryLevel
+        /// </summary>
+        /// <param name="level"> The SupervisoryLevel to set. </param>
+        public void SetEmploymentResponsibilityLevel(SupervisoryLevel level)
+        {
+            Level = level;
+        }
+
+        /// <summary>
+        /// Override ToString.
+        /// 
+        /// Returns a string representing this instance of Employment as a csv (comma separated value).
+        /// 
+        /// Format: Title, Level, MMM,dd,yyyy, Years
+        /// </summary>
+        /// <returns> A string in CSV format. </returns>
+        public override string ToString()
+        {
+            return $"{Title},{Level},{StartDate.ToString("MMM,dd,yyyy")},{Years}";
+        }
+
+        /// <summary>
+        /// This method validates our StartDate as no validation is present in the set method.
+        /// </summary>
+        /// <param name="startDate"> The DateTime to set StarteDate to and validate</param>
+        /// <exception cref="ArgumentException"> startDate can not be in the future. </exception>
+        public void CorrectStartDate(DateTime startDate)
+        {
+            if (startDate >= DateTime.Today.AddDays(1))
+            {
+                throw new ArgumentException($"The start date {startDate} is in the future.");
+            }
+
+            //Else not necessary as the throw will exit my constructor.
+            StartDate = startDate;
+
+            TimeSpan span = DateTime.Now - startDate;
+            Years = Math.Round((span.Days / 365.25), 1); //365.25 accounts for leap years
+        }
+
+        #endregion
+    }
+}
+//End of Lesson 1.1
---
+++ OOPsReview/OOPsReview/Utilities.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Checks if our value is positive.
+        /// 0.0 is assumed positive.
+        /// </summary>
+        /// <param name="value">The double value to check.</param>
+        /// <returns>True if the value is positive, false otherwise.</returns>
+        public static bool IsPositive(double value)
+        {
+            /*
+            if(value >= 0.0)
+            {
+                return true;
+            }
+
+            return false;
+            */
+
+            /*
+            bool valid = false;
+
+            if(value >= 0.0)
+            {
+                valid = true;
+            }
+
+            return valid;
+            */
+
+            return value >= 0.0;
+        }
+
+        /// <summary>
+        /// Checks if our value is positive.
+        /// 0 is assumed positive.
+        /// </summary>
+        /// <param name="value">The int value to check.</param>
+        /// <returns>True if the value is positive, false otherwise.</returns>
+        public static bool IsPositive(int value) => value >= 0;
+
+        /// <summary>
+        /// Checks if our value is positive.
+        /// 0 is assumed positive.
+        /// </summary>
+        /// <param name="value">The decimal value to check.</param>
+        /// <returns>True if the value is positive, false otherwise.</returns>
+        public static bool IsPositive(decimal value) => value >= 0.0m;
+
+    }
+}
